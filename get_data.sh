39926f9745c2891cbbe8d0f26f0d859b358c42ee
Leslie Emery <leslie.s.emery@gmail.com>
2021-04-15 19:56:17 -0700

    Add data download script

---
+++ get_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Download The Met data
+wget -P data https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv
+
+# Download the MoMA data
+wget -P data https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.csv
