98e31465899e6e9a52a8f14d9c76561b963e0225
Leslie Emery <leslie.s.emery@gmail.com>
2021-04-17 15:29:35 -0700

    Start to work on SPL checkout data

---
+++ convert_to_jupyter_notebooks.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Convert to jupyter notebook
+# jupytext --to notebook 01_clean_data.py
+jupytext --to notebook --execute 01_clean_data.py
--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env zsh
 
+# Download SPL book checkout data
+wget -O data/spl_checkouts.csv https://data.seattle.gov/resource/tmmm-ytt6.csv
+head -n 20 data/spl_checkouts.csv > data/spl_checkouts_head.csv
+# https://data.seattle.gov/Community/Checkouts-by-Title/tmmm-ytt6
+
 # Download The Met data
 wget -P data https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv
+head -n 20 data/MetObjects.csv > data/MetObjects_head.csv
 
 # Download the MoMA data
 wget -P data https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.csv
