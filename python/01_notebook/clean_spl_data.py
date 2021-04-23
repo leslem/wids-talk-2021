d2b9a00fd6a355eb7e6f58650addf2c951c221af
Leslie Emery <leslie.s.emery@gmail.com>
2021-04-22 21:54:56 -0700

    Add python dir structure and placeholder files

--- python/01_notebook/clean_spl_data.py
+++ python/01_notebook/clean_spl_data.py
@@ -3,8 +3,9 @@
 import numpy as np
 import pandas as pd
 
+
 # Read in data
-met_objects = pd.read_csv("data/spl_checkouts.csv", nrows=20)
+spl_objects = pd.read_csv('../data/spl_checkouts_2020_12.csv')
 
 # Examine data
-met_objects.info()
+.info()
